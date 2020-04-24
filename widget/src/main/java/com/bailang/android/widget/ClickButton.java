f6bd5923394a9c5687a391aa5be974b2967a4ad8
Bailang <32005655+GoldDevTeam@users.noreply.github.com>
2020-04-25 01:55:04 +0800

    Init Project

---
+++ widget/src/main/java/com/bailang/android/widget/ClickButton.java
@@ -0,0 +1,33 @@
+package com.bailang.android.widget;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.appcompat.widget.AppCompatTextView;
+
+public class ClickButton extends AppCompatTextView {
+
+    public ClickButton(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        applyTypeface(context, attrs);
+    }
+
+    public ClickButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        applyTypeface(context, attrs);
+    }
+
+    public ClickButton(Context context) {
+        super(context);
+    }
+
+    @SuppressLint("ResourceType")
+    private void applyTypeface(Context context, AttributeSet attrs){
+        final String androidNamespace = "http://schemas.android.com/apk/res/android";
+        String textColor = attrs.getAttributeValue(androidNamespace, "textColor");
+        if(textColor == null || textColor.equals("")) {
+            setTextColor(context.getResources().getColorStateList(R.drawable.btn_text));
+        }
+    }
+}
---
+++ widget/src/main/java/com/bailang/android/widget/RatioImageView.java
@@ -0,0 +1,32 @@
+package com.bailang.android.widget;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
+
+import androidx.appcompat.widget.AppCompatImageView;
+
+public class RatioImageView extends AppCompatImageView {
+    public RatioImageView(Context context) {
+        super(context);
+    }
+
+    public RatioImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public RatioImageView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        Drawable d = getDrawable();
+        if (d != null) {
+            int w = MeasureSpec.getSize(widthMeasureSpec);
+            int h = w * d.getIntrinsicHeight() / d.getIntrinsicWidth();
+            setMeasuredDimension(w, h);
+        }
+        else super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+}
